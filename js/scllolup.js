de0ca05f5a5bdd353344c1b224086b84ebeeec98
kei9992 <96216691+kei9992@users.noreply.github.com>
2022-06-06 20:36:34 +0900

    Add files via upload

---
+++ js/scllolup.js
@@ -0,0 +1,18 @@
+ScrollReveal().reveal('.uptitle1', { 
+    duration: 1500, // アニメーションの完了にかかる時間
+    viewFactor: 0.9, // 0~1,どれくらい見えたら実行するか
+    reset: true ,  // 何回もアニメーション表示するか
+    origin: 'bottom', 
+    distance: '50px',
+    
+    
+  });
+  ScrollReveal().reveal('.uptitle2', { 
+    duration: 2000, // アニメーションの完了にかかる時間
+    viewFactor: 0.9, // 0~1,どれくらい見えたら実行するか
+    reset: true ,  // 何回もアニメーション表示するか
+    origin: 'bottom', 
+    distance: '50px',
+    
+    
+  });
